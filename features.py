1e4d3ce49ee71f2fa4c548806a79095332dab402
thelokpick <liamkinney@DN51ussu.SUNet>
2016-05-17 16:17:49 -0700

    add_discourse_markers_feature
    Added “add_discourse_markers_feature”  and supporting functions and
    dictionary

--- features.py
+++ features.py
@@ -15,8 +15,45 @@ from collections import Counter
     data_point["content"]["comments"][i]["body"] - contains the actual text of the ith comment
 """
 
-def add_discourse_markers_features(data_point, features)):
-	hi = "hi"
+disagreement_markers = {
+	"really": 67,
+	"no": 66,
+	"actually": 60,
+	"but": 58,
+	"so": 58,
+	"you mean": 57
+}
+
+agreement_markers = {
+	"yes": 73,
+	"i know": 64,
+	"i believe": 62,
+	"i think": 61,
+	"just": 57
+}
+
+
+# returns the size of the intersection of the disagreement markers keys and the comment
+def disagreement_measure(comment):
+	com_list = comment.lower().split()
+	return len(set(com_list).intersection(disagreement_markers.keys()))
+
+
+# returns the size of the intersection of the agreement markers keys and the comment
+def agreement_measure(comment):
+	com_list = comment.lower().split()
+	return len(set(com_list).intersection(agreement_markers.keys()))
+
+
+def add_discourse_markers_features(data_point, features):
+	op_text = data_point["op_text"]
+	root_reply = data_point["content"]["comments"][0]["body"]
+	last_reply = data_point["content"]["comments"][-1]["body"]
+
+	features["disagree_factor:", disagreement_measure(root_reply)] += 1.0
+	features["agree_factor:", agreement_measure(last_reply)] += 1.0
+
+
 
 
 def add_words_in_common_features(data_point, features):
@@ -34,7 +71,7 @@ def test_phi(data_point):
     length = min(4, len(comments))
     features['len:' + str(length)] += 1.0
     add_words_in_common_features(data_point, features)
-    add_discourse_markers_features(data_point, features))
+    #add_discourse_markers_features(data_point, features)
     return features
 
 
