bab005eb163c09cedbb795f6db51f7d00ce44e9c
Gabe Alvarez <Gabe Alvarez>
2016-05-17 13:42:10 -0700

    Load from json

--- load_data.py
+++ load_data.py
@@ -1,1 +1,14 @@
-import pickle
+import json
+
+TRAIN_OP_DATA = "cmv/op_task/train_op_data.jsonlist"
+
+def load_data():
+	f = open(TRAIN_OP_DATA, "r")
+	data = []
+	for line in f:
+		data.append(json.loads(line))
+	print data[1]['delta_label']
+
+
+if __name__ == "__main__":
+	load_data()
